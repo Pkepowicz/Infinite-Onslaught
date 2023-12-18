fa299aeea9c6c243c7c0a667f35dce858f85efef
pkepowicz <paw.kepowicz@gmail.com>
2023-12-18 22:31:52 +0100

    Added shooting cooldown

--- assets/player/Player.gd
+++ assets/player/Player.gd
@@ -8,6 +8,7 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var username : String
+@onready var attack_cooldown = $GunRotation/Timer
 
 var sync_pos = Vector2(0,0)
 var sync_rot = 0
@@ -27,9 +28,11 @@ func _physics_process(delta):
 		$GunRotation.rotation_degrees = lerpf($GunRotation.rotation_degrees, sync_rot, 0.4)
 		return
 	$GunRotation.look_at(get_viewport().get_mouse_position())
-	if Input.is_action_just_pressed("Fire"):
+	if Input.is_action_just_pressed("Fire") && attack_cooldown.is_stopped():
+		attack_cooldown.start()
 		print(username)
 		Fire.rpc()
+		
 	sync_pos = global_position
 	sync_rot = $GunRotation.rotation_degrees
 	var direction_x = Input.get_axis("ui_left", "ui_right")
