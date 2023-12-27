a90c5e6f779a68e29ca7f31b5c5b643db0e1853d
pkepowicz <paw.kepowicz@gmail.com>
2023-12-27 17:08:28 +0100

    Positioning changes for export

--- assets/player/weapons/Bullet.gd
+++ assets/player/weapons/Bullet.gd
@@ -11,8 +11,14 @@ var damage: int
 func _ready():
 	direction = Vector2(1,0).rotated(rotation)
 	$DamageBox.set_parent(parent)
+	delete_on_timer(5)
 
 func _physics_process(delta):
 	velocity = SPEED * direction
 
 	move_and_slide()
+
+func delete_on_timer(seconds):
+	await get_tree().create_timer(seconds).timeout
+	queue_free()
+	
