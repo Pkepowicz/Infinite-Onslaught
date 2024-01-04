b8caaaea8d8b6c04a62be5a03f65bcf104a26d25
ddziechciarz <alawon12@gmail.com>
2024-01-04 20:15:33 +0100

    added visual indicator

--- assets/PickUps/PickupComponent.gd
+++ assets/PickUps/PickupComponent.gd
@@ -5,5 +5,4 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.get_parent().has_method("pickup"):
 		var returned_node = area.get_parent().pickup()
 		var node_to_add = returned_node.instantiate()
-		node_to_add.position = get_parent().position
 		get_parent().add_child(node_to_add)
